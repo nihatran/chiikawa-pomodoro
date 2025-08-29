be44796acb0f93be33dced6fcebb9b7034f0e694
nihatran <89045243+nihatran@users.noreply.github.com>
2025-08-28 23:11:09 -0400

    Add start/reset buttons and adjust sizes to ensure app works on most devices

--- src/Options.tsx
+++ src/Options.tsx
@@ -3,7 +3,7 @@ import "./Options.css";
 export function Options() {
   return (
     <>
-      <button onClick={() => console.log("allo")} className="optionsbtn">
+      <button onClick={() => console.log("allo")} className="optionsBtn">
         <img className="star" src="chiikawastar.png"></img>
       </button>
     </>
